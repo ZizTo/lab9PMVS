3f319b447fdae9f2c71b173e48f4a82e21adb983
ZizTo <pashamatievski@gmail.com>
2026-05-19 14:23:57 +0300

    Test fixes

--- Task1-WeatherApplication/app/src/commonTest/kotlin/com/example/weatherapplication/WeatherStateHolderTest.kt
+++ Task1-WeatherApplication/app/src/commonTest/kotlin/com/example/weatherapplication/WeatherStateHolderTest.kt
@@ -1,11 +1,30 @@
 package com.example.weatherapplication
 
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
-import kotlin.test.assertTrue
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class WeatherStateHolderTest {
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
 
     @Test
     fun testCitiesListInitializesCorrectly() {
@@ -24,7 +43,7 @@ class WeatherStateHolderTest {
         assertTrue(stateHolder.cities.contains("Лондон"))
 
         stateHolder.addCity("Лондон")
-        assertEquals(3, stateHolder.cities.size, "Количество городов не должно было измениться")
+        assertEquals(3, stateHolder.cities.size)
     }
 
     @Test
